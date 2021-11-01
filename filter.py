56246e102fdc98300622a81308690775d29d4efd
Matthew Dixson <mdixson2@vols.utk.edu>
2021-11-01 15:03:40 -0400

    Filter finished

---
+++ filter.py
@@ -0,0 +1,29 @@
+#Filter for game titles to remove NSFW content
+def filter_titles(fn):
+    FILTER_LIST = ['18+', 'sex', 'fuck', 'hentai', 'dick', 'shit', 'cock', 'cum']
+    with open(fn, 'r', encoding='utf-8') as f:
+        data = f.readlines()
+    
+    for i in range(len(data)):
+        data[i] = data[i].lower()
+    
+    l = []
+    for d in data:
+        for f in FILTER_LIST:
+             if f not in d:
+                 l.append([d])
+                 break
+    x = []
+    for d in data:
+        for f in FILTER_LIST:
+             if f in d:
+                 x.append([d])
+                 break
+    print(len(x))
+    print(len(l))
+    with open('SteamScraper/filtered.txt', 'w', encoding='utf-8') as f:
+        for i in l:
+            f.write(i[0])
+
+
+filter_titles("SteamScraper/id_mappings.txt")
--- scraper.py
+++ scraper.py
@@ -1,10 +1,16 @@
 import requests
 API_KEY = "D331F1DEB5EDAE0D7CC90FA90B98852E"
 #r = requests.get("http://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?appid=440&count=3&maxlength=300&format=json")
-r = requests.get("http://api.steampowered.com/ISteamApps/GetAppList/v2/?key=" + API_KEY)
+#r = requests.get("http://api.steampowered.com/ISteamApps/GetAppList/v2/?key=" + API_KEY)
+#print(r.status_code)
+#data = r.json()
 
-data = r.json()
-
-with open('SteamScraper/id_mappings.txt', 'w', encoding='utf-8') as f:
+"""with open('SteamScraper/id_mappings.txt', 'w', encoding='utf-8') as f:
     for app in data['applist']['apps']:
         f.write(str(app['name']) + ', ' + str(app['appid']) + '\n')
+"""
+r = requests.get("http://api.steampowered.com/ISteamEconomy/GetAssetPrices/v0001/?key=" + API_KEY + "&appid=240")
+
+data = r.status_code
+print(data)
+
