da2ab1fb14c19435fd9efc23619cf151d77c6cde
Matthew Dixson <mdixson2@vols.utk.edu>
2021-11-15 14:29:53 -0500

    DLC done

--- matthew/dlc.py
+++ matthew/dlc.py
@@ -1,41 +1,57 @@
 from bs4 import BeautifulSoup
 import requests
+import multiprocessing as mp
 
-id_file = open('SteamScraper/final/filtered_titles.txt', 'r', encoding='utf-8')
-dlc_file = open('test_dlc.txt', 'w', encoding='utf-8')
 
-for line in id_file:
-    if line == '':
-        continue
-    elif line == '+':
-        continue
 
-    game_name = line.split(',')[0]
-    game_id = line.split(',')[1]
-    dlc_file.write('\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n')
-    dlc_file.write(game_name + ', ' + game_id)
+def dlc(game_name, game_id):
+    dlc_file = open('test_dlc.txt', 'a', encoding='utf-8')
+    i = 0
 
-    game_id = str(281990)
+    
+
     URL = 'https://store.steampowered.com/app/' + game_id
     page = requests.get(URL)
 
     soup = BeautifulSoup(page.content, 'html.parser')
     dlc_table = soup.find(class_='gameDlcBlocks')
 
-    #print(language_table)
+    # print(language_table)
     if dlc_table is None:
-        continue
-    dlc_prices = dlc_table.find_all(class_ ='game_area_dlc_price')
-    dlc_names = dlc_table.find_all(class_ ='game_area_dlc_name')
-    print(dlc_prices)
-   
-    dlcs = [(name, price) for name in dlc_names for price in dlc_prices]
-   
-    print(dlc_names)
-    for dlc in dlcs:
+        #dlc_file.write('\n')
+        return
+
+    dlc_prices = dlc_table.find_all(class_='game_area_dlc_price')
+    dlc_names = dlc_table.find_all(class_='game_area_dlc_name')
+    dlcs = [(name.text.strip(), price.text.strip())
+               for name, price in zip(dlc_names, dlc_prices)]
+    if len(dlcs) != 0:
+        dlc_file.write(game_name + ', ' + game_id)
+    for dlc in dlcs: 
         if dlc == '':
             continue
-        #dlc_file.write(dlc[0].strip() + ', ' +  dlc[1].strip() + '\n')
-    break;
-dlc_file.close()
-id_file.close()
+        dlc_file.write(', ' + dlc[0] + ', ' + dlc[1])
+    dlc_file.write('\n')
+    dlc_file.close()
+
+def main():
+    id_file = open('SteamScraper/final/filtered_titles.txt', 'r', encoding='utf-8')
+
+    arguments = []
+    for line in id_file:
+        if line == '':
+            continue
+        elif line == '+':
+            continue
+
+        game_name = line.split(',')[0].strip()
+        game_id = line.split(',')[1].strip()
+        arguments.append((game_name, game_id))
+    
+    with mp.Pool(5) as p:
+        p.starmap(dlc, arguments)
+#dlc(game_name, game_id)
+    id_file.close()
+
+if __name__ == "__main__":
+    main()
